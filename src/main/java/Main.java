431d08340c28d1fda8a582c83c4ba99122ef0627
gabrieltomazine006 <gabriel.nascimento@ucsal.edu.br>
2025-09-01 18:56:32 -0300

    ATIVIDADE POOA SOLID - OCP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,21 @@
-import br.com.mariojp.solid.ocp.*;
 
+import java.util.Map;
+
+import br.com.mariojp.solid.ocp.CustomerType;
+import br.com.mariojp.solid.ocp.DiscountCalculator;
+import br.com.mariojp.solid.ocp.DiscountPolicy;
+import br.com.mariojp.solid.ocp.PartnerPolicy;
+import br.com.mariojp.solid.ocp.PremiumPolicy;
+import br.com.mariojp.solid.ocp.RegularPolicy;
 public class Main {
-	public static void main(String[] args) {
-		DiscountCalculator calc = new DiscountCalculator();
-		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
-		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
-		// PARTNER deveria ter 12%
-		System.out.println("PARTNER 100 -> " + calc.apply(100, CustomerType.PARTNER));
-	}
+    public static void main(String[] args) {
+        Map<CustomerType, DiscountPolicy> policies = Map.of(
+            CustomerType.REGULAR, new RegularPolicy(),
+            CustomerType.PREMIUM, new PremiumPolicy(),
+            CustomerType.PARTNER, new PartnerPolicy()
+        );
+        DiscountCalculator calculator = new DiscountCalculator(policies);
+        double amount = 100;
+        System.out.println(calculator.apply(amount, CustomerType.PARTNER)); // Deve imprimir 88.0
+    }
 }
